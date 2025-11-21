8240edb5ec756c21747e1756533e442106a055fa
Rohit kumar Sahu <114480322+rohitkrsahu04@users.noreply.github.com>
2025-11-21 20:38:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-let menuIcon = document.querySelector('#menu-icon');
+// Mobile Menu Toggle
+let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
+menu.onclick = () => {
     navbar.classList.toggle('active');
 };
+
+// Typing Effect
+var typed = new Typed('.typing', {
+    strings: ["Software Engineer", "Web Developer", "ML Enthusiast"],
+    typeSpeed: 80,
+    backSpeed: 40,
+    loop: true
+});
